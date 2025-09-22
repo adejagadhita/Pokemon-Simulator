818536e8e64c512d03387b406f49705186b26976
Your Name <nymsitamanikjayanti@gmail.com>
2025-09-22 15:20:27 +0800

    sitaupdate

--- src/contenLeft.jsx
+++ src/contenLeft.jsx
@@ -1,5 +1,70 @@
-function Left() {
+import React from "react";
 
+// import asset kartu untuk kiri
+import cardLeft1 from "./assets/ungu1.png";
+import cardLeft2 from "./assets/ungu2.png";
+import cardLeft3 from "./assets/ungu3.png";
+import cardLeft4 from "./assets/ungu4.png";
+import cardLeft5 from "./assets/ungu5.png";
+
+import forbidIcon from "./assets/BanCard.png";
+
+//  komponen utama khusus kiri
+export default function ContenLeft() {
+  const leftCards = [cardLeft1, cardLeft2, cardLeft3, cardLeft4, cardLeft5];
+
+  return (
+    <div className="md:col-span-3 relative">
+      {/* Ban button di atas card pertama */}
+      <BanButton
+        icon={forbidIcon}
+        side="left"
+        className="absolute -top-14 left-8 -translate-x-1/2"
+      />
+
+      {/* List kartu trainer kiri */}
+      <aside className="p-2">
+        <div className="space-y-2">
+          {leftCards.map((src, idx) => (
+            <Row key={`left-${idx}`} img={src} altText={`Trainer ${idx + 1}`} />
+          ))}
+        </div>
+      </aside>
+    </div>
+  );
 }
 
-export default Left;
+// komponen baris kartu
+function Row({ img, altText }) {
+  return (
+    <div className="flex justify-center">
+      <img
+        src={img}
+        alt={altText}
+        draggable="false"
+        className="w-full max-w-[180px] md:max-w-[200px] select-none drop-shadow-[0_6px_12px_rgba(0,0,0,0.25)]"
+      />
+    </div>
+  );
+}
+
+// komponen BanButton
+function BanButton({ icon, side = "left", className = "" }) {
+  return (
+    <div
+      className={
+        "z-20 h-14 w-14 md:h-16 md:w-16 rounded-xl " +
+        "flex items-center justify-center " +
+        className
+      }
+      title={side === "left" ? "Ban kiri" : "Ban kanan"}
+    >
+      <img
+        src={icon}
+        alt="ban"
+        className="h-12 w-12 md:h-12 md:w-12 object-contain select-none"
+        draggable="false"
+      />
+    </div>
+  );
+}
--- src/contenRight.jsx
+++ src/contenRight.jsx
@@ -1,5 +1,72 @@
-function Right() {
 
+
+import React from "react";
+
+// import asset kartu untuk kanan
+import cardRight1 from "./assets/orange6.png";
+import cardRight2 from "./assets/orange7.png";
+import cardRight3 from "./assets/orange8.png";
+import cardRight4 from "./assets/orange9.png";
+import cardRight5 from "./assets/orange10.png";
+
+import forbidIcon from "./assets/BanCard.png";
+
+//  komponen utama khusus kanan
+export default function ContenRight() {
+  const rightCards = [cardRight1, cardRight2, cardRight3, cardRight4, cardRight5];
+
+  return (
+    <div className="md:col-span-3 relative">
+      {/* Ban button di atas card pertama */}
+      <BanButton
+        icon={forbidIcon}
+        side="right"
+        className="absolute -top-14 -right-6 -translate-x-1/2"
+      />
+
+      {/* List kartu trainer kanan */}
+      <aside className="p-2">
+        <div className="space-y-2">
+          {rightCards.map((src, idx) => (
+            <Row key={`right-${idx}`} img={src} altText={`Trainer ${6 + idx}`} />
+          ))}
+        </div>
+      </aside>
+    </div>
+  );
 }
 
-export default Right;
+// komponen baris kartu
+function Row({ img, altText }) {
+  return (
+    <div className="flex justify-center">
+      <img
+        src={img}
+        alt={altText}
+        draggable="false"
+        className="w-full max-w-[180px] md:max-w-[200px] select-none drop-shadow-[0_6px_12px_rgba(0,0,0,0.25)]"
+      />
+    </div>
+  );
+}
+
+// komponen BanButton
+function BanButton({ icon, side = "right", className = "" }) {
+  return (
+    <div
+      className={
+        "z-20 h-14 w-14 md:h-16 md:w-16 rounded-xl " +
+        "flex items-center justify-center " +
+        className
+      }
+      title={side === "left" ? "Ban kiri" : "Ban kanan"}
+    >
+      <img
+        src={icon}
+        alt="ban"
+        className="h-12 w-12 md:h-12 md:w-12 object-contain select-none"
+        draggable="false"
+      />
+    </div>
+  );
+}
