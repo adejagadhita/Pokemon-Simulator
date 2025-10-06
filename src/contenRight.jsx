8a4ae2591895b04cd91792b05748087cf952c4d7
Your Name <nymsitamanikjayanti@gmail.com>
2025-10-06 15:53:51 +0800

    revisikakdwikyyy

--- src/contenRight.jsx
+++ src/contenRight.jsx
@@ -5,9 +5,7 @@ import cardRight3 from "./assets/orange8.png";
 import cardRight4 from "./assets/orange9.png";
 import cardRight5 from "./assets/orange10.png";
 
-export default function ContenRight({
- 
-}) {
+export default function ContenRight({ pokemons = [] }) {
   const rightCards = [cardRight1, cardRight2, cardRight3, cardRight4, cardRight5];
 
   return (
@@ -17,8 +15,8 @@ export default function ContenRight({
           <Row
             key={idx}
             bg={bg}
-            altText={`Trainer ${6 + idx}`}
-            
+            altText={`Trainer ${idx + 6}`} // tim kanan mulai Trainer 6
+            pokemon={pokemons[idx]}
           />
         ))}
       </div>
@@ -26,18 +24,26 @@ export default function ContenRight({
   );
 }
 
-function Row({ bg, altText  }) {
+function Row({ bg, altText, pokemon }) {
   return (
-    <div className="relative flex justify-center" >
+    <div className="relative flex justify-center">
+      
       <img
         src={bg}
         alt={altText}
-        draggable="false"
         className="w-full max-w-[180px] md:max-w-[200px] select-none drop-shadow-[0_6px_12px_rgba(0,0,0,0.25)]"
       />
 
-
-
+      {/* Pokemon yang dipick */}
+      {pokemon && (
+        <div className="absolute inset-0 flex items-center justify-center overflow-hidden p-2">
+        <img
+          src={pokemon.image}
+          alt={pokemon.name}
+          className="absolute bottom-1 right-1 w-[40%]"
+        />
+        </div>
+      )}
     </div>
   );
 }
